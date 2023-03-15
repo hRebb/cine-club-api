3641270fcb15f1ca3c8790ccd5f5272fc680639a
hamzaR <hamza.rebb@gmail.com>
2023-03-15 20:13:26 +0100

    get and write movies

--- movie.py
+++ movie.py
@@ -1,3 +1,9 @@
+import json
+import os
+
+CUR_DIR = os.path.dirname(__file__)
+DATA_FILE = os.path.join(CUR_DIR, "data", "movies.json")
+
 class Movie:
     def __init__(self, title) -> str:
         self.title = title.title()
@@ -5,7 +11,17 @@ class Movie:
     def __str__(self) -> str:
         return f"{self.title}"
     
+    @staticmethod
+    def _get_movies(self):
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+
+    @staticmethod
+    def _write_movies(movies):
+        with open(DATA_FILE, 'w') as f:
+            json.dump(movies, f, indent=4)
 
 if __name__ == "__main__":
-    m = Movie("harry potter")
-    print(m)
+    m = Movie("Le seigneur des anneaux")
+    print(m._write_movies(["Alice in wonderland", "Mary Poppins"]))
+    #print(m)
