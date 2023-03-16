ab187c7f4df880178ae5ddedc3514fd883186590
hamzaR <hamza.rebb@gmail.com>
2023-03-16 17:58:39 +0100

    start to make the add movie method

--- movie.py
+++ movie.py
@@ -1,5 +1,6 @@
 import json
 import os
+import logging
 
 CUR_DIR = os.path.dirname(__file__)
 DATA_FILE = os.path.join(CUR_DIR, "data", "movies.json")
@@ -21,7 +22,18 @@ class Movie:
         with open(DATA_FILE, 'w') as f:
             json.dump(movies, f, indent=4)
 
+    @classmethod
+    def add_to_movies(cls, title):
+        movies = cls._get_movies()
+        if title.title() not in movies:
+            movies.append(title.title())
+            cls._write_movies(movies)
+            logging.info(f"{title} a été ajouté dans la liste des films.")
+        else:
+            logging.info(f"{title} est déjà dans la liste de films.")
+
 if __name__ == "__main__":
     m = Movie("Le seigneur des anneaux")
-    print(m._write_movies(["Alice in wonderland", "Mary Poppins"]))
-    #print(m)
+    # print(m._write_movies(["Alice in wonderland", "Mary Poppins"]))
+    # print(m)
+    # m.add_to_movies()
