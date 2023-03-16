8e620991d6d890e34c4d747cb3dc9cdbe259e9f3
hamzaR <hamza.rebb@gmail.com>
2023-03-16 18:54:00 +0100

    setup the ui

---
+++ app.py
@@ -0,0 +1,26 @@
+from PySide2 import QtWidgets
+
+class App(QtWidgets.QWidget):
+    def __init__(self):
+        super(App, self).__init__()
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.setWindowTitle("Py Cinema Club")
+        self.lineEdit = QtWidgets.QLineEdit(self)
+        self.add_button = QtWidgets.QPushButton("Add")
+        self.list_widget = QtWidgets.QListWidget(self)
+        self.rm_button = QtWidgets.QPushButton("Remove")
+
+        self.layout.addWidget(self.lineEdit)
+        self.layout.addWidget(self.add_button)
+        self.layout.addWidget(self.list_widget)
+        self.layout.addWidget(self.rm_button)
+
+app = QtWidgets.QApplication([])
+
+win = App()
+win.show()
+
+app.exec_()
