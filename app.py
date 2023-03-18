623c56c2dd76fcc35e597f0a95c3577bee7ab507
hamzaR <hamza.rebb@gmail.com>
2023-03-18 10:03:57 +0100

    add connection to film list in json

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from PySide2 import QtWidgets
+from movie import get_movies
 
 class App(QtWidgets.QWidget):
     def __init__(self):
         super(App, self).__init__()
         self.setup_ui()
+        self.populate_movies()
 
     def setup_ui(self):
         self.layout = QtWidgets.QVBoxLayout(self)
@@ -17,6 +19,11 @@ class App(QtWidgets.QWidget):
         self.layout.addWidget(self.add_button)
         self.layout.addWidget(self.list_widget)
         self.layout.addWidget(self.rm_button)
+
+    def populate_movies(self):
+        self.movies = get_movies()
+        for movie in self.movies:
+            self.list_widget.addItem(movie.title)
 
 app = QtWidgets.QApplication([])
 
